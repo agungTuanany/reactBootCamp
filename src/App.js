3f12db9d878c212abfbc9268c7ee865bb3f695e4
agungTuanany <agung.tuanany@gmail.com>
2019-05-12 11:02:52 +0700

    Handling an Event on React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  clikcedItem() {
+    alert('clicked!');
+  }
+
+  mouseOvered() {
+    alert('overed Mouse');
+  }
+
+  onChange(e) {
+    console.log(e.target.value);
+  }
+
+  submitted(e) {
+    e.preventDefault();
+
+    alert('submitted');
+  }
+
+
+  render() {
   const list = [
     'item 1',
     'item 2',
     'another item'
   ];
+    return (
+      <div className="App">
+        <h1>Handling an Event in Reactjs / Event-Handler </h1>
+        <h2>
+          {
+            list.map(item => {
+              return (
+                <div key={item}>
+                  <div onClick={this.clikcedItem}>{item}</div>
+                  {/*
+                  <div onMouseOver={this.mouseOvered}>{item}</div>
+                  */}
+                </div>
+              )
+            })
+          }
+        </h2>
+        <form onSubmit={this.submitted}>
+          <input onChange={this.onChange} />
+        </form>
+      </div>
 
-  return (
-    <div className="App">
-      <h1>
-        {
-          list.map(item => {
-            return <div>{item}</div>
-          })
-        }
-      </h1>
-    </div>
-
-  );
+    );
+  }
 }
 
 export default App;
