3b0303543297f62c64e7616ce6e0b8708ce33146
agungTuanany <agung.tuanany@gmail.com>
2019-05-12 13:21:55 +0700

    State

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    // whenever you pass in a method of the component to a element or to
-    // a component you need to bind with '(this)'.
+
+    this.state = {
+      title: "App title",
+      author: 'agung Tuanany'
+    }
+
    this.submitted = this.submitted.bind(this);
   }
 
@@ -24,20 +28,8 @@ class App extends Component {
   ];
     return (
       <div className="App">
-        <h1>Handling an Event in Reactjs / Event-Handler </h1>
-        <h2>
-          {
-            list.map(item => {
-              return (
-                <div key={item} onClick={this.clikcedItem}>{item}</div>
-              )
-            })
-          }
-        </h2>
-        <form onSubmit={this.submitted}>
-          {/*in React a "ref=" attributes in element recommended to put a function   */}
-          <input onChange={this.onChange} ref={input => this.input = input} />
-        </form>
+        <h1>{this.state.title}</h1>
+        <p>{this.state.author}</p>
       </div>
 
     );
