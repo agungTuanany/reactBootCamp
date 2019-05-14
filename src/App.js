f0ac826aa772c038f084108c87e09af6b4038936
agungTuanany <agung.tuanany@gmail.com>
2019-05-14 21:57:26 +0700

    Simple Crud app: GET 01.Basic setup

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
 import React,{ Component } from 'react';
 import './App.css';
 
-import Profile from './components/myComponent'
+const products = [
+  {
+    id: 1,
+    name: 'Samsung x10',
+    price: 400
+  },
+  {
+    id: 2,
+    name: 'samsung x9',
+    price: 300
+  }
+];
 
+localStorage.setItem('products', JSON.stringify(products));
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      title: "App title",
-      author: 'agung Tuanany'
-    }
-
-   this.onClick = this.onClick.bind(this);
+      product: []
+    };
   }
 
-  onClick(e) {
-    this.setState({
-      title: "A new title",
-      author: "AGUNG TUANANY"
-    });
+  componentWillMount() {
+    const products = JSON.parse(localStorage.getItem('products'));
+
+    this.setState({ products })
   }
 
   render() {
     return (
       <div className="App">
-        <h1>{this.state.title}</h1>
-        <p>{this.state.author}</p>
-        <div onClick={this.onClick}>click me</div>
+        <h1>Products Manager</h1>
+        {
+          this.state.products.map(product => {
+            return (
+              <div key={product.id} >
+                <span>{product.name}</span> | <span>{product.price}</span>
+              </div>
 
-        <Profile
-          title= "kitchen set area"
-          company= "dapur mama imma"
-        />
+            );
+          })
+        }
       </div>
     );
   }
